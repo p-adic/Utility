4965f107cbcbeb73e0b4e8c678712d085521dea2
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2025-06-19 21:28:07 +0900

    Add files via upload

---
+++ StdStream/__float128/a.hpp
@@ -0,0 +1,6 @@
+// c:/Users/user/Documents/Programming/Utility/StdStream/__float128/a.hpp
+
+#pragma once
+
+template <class Traits> inline basic_ostream<char,Traits>& operator<<( basic_ostream<char,Traits>& os , const __float128& x );
+
---
+++ StdStream/__float128/a_Body.hpp
@@ -0,0 +1,6 @@
+// c:/Users/user/Documents/Programming/Utility/StdStream/__float128/a_Body.hpp
+
+#pragma once
+#include "a.hpp"
+
+template <class Traits> inline basic_ostream<char,Traits>& operator<<( basic_ostream<char,Traits>& os , const __float128& x ) { return os << ld{ x }; }
